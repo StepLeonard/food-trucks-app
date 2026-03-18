12ef8f3b5f563b926db1fd00c3c89501223fe47f
stepleonard <stephanie.leonard716@gmail.com>
2026-03-18 13:16:45 -0700

    added food-trucks-count

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,35 +3,52 @@ import { useEffect, useState } from "react";
 
 function Home() {
   const [foodTrucks, setFoodTrucks] = useState([]);
+  const [foodTruckCount, setFoodTruckCount] = useState(0);
 
   async function getAllFoodTrucks() {
     const response = await fetch("/api/get-all-food-trucks");
     const data = await response.json();
-    console.log("DATA:", data);
+    console.log("ALL TRUCKS:", data);
     setFoodTrucks(data);
+  }
+
+  async function getFoodTruckCount() {
+    const response = await fetch("/api/get-food-trucks-count");
+    const data = await response.json();
+    console.log("COUNT:", data);
+
+   
+    setFoodTruckCount(data.count);
   }
 
   useEffect(() => {
     getAllFoodTrucks();
+    getFoodTruckCount();
   }, []);
 
   return (
-    <div className="container">
-      <h1>Food Trucks 🚚</h1>
+  <div className="container">
+    <h1>Food Trucks 🚚</h1>
 
-      <div className="grid">
-        {foodTrucks.map((truck) => (
-          <div className="card" key={truck.id}>
-            <h2>{truck.name}</h2>
-            <p>{truck.slogan}</p>
-            <p>Location: {truck.current_location}</p>
-            <p>Price Level: {truck.price_level}</p>
-            <p>Rating: {truck.rating}</p>
-          </div>
-        ))}
-      </div>
+    <h2>
+  We have <span className="count">{foodTruckCount}</span> available choices!
+</h2>
+
+    <div className="grid">
+      {foodTrucks.map((truck) => (
+        <div key={truck.id} className="card">
+          <h3>{truck.name}</h3>
+          <p>Location: {truck.current_location}</p>
+          <p>Daily Special: {truck.daily_special}</p>
+          <p>Slogan: {truck.slogan}</p>
+          <p>Vegan Options: {truck.has_vegan_options ? "Yes" : "No"}</p>
+          <p>Price Level: {truck.price_level}</p>
+          <p>Rating: {truck.rating}</p>
+        </div>
+      ))}
     </div>
-  );
+  </div>
+);
 }
 
 export default Home;
